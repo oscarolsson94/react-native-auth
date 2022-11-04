1500644ec8c0e0991404ab5c46a499b7dab3ce26
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-11-04 15:46:06 +0100

    handling loading state

--- components/ui/LoadingOverlay.js
+++ components/ui/LoadingOverlay.js
@@ -1,6 +1,6 @@
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 
-function LoadingOverlay({ message }) {
+export function LoadingOverlay({ message }) {
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.message}>{message}</Text>
@@ -9,13 +9,11 @@ function LoadingOverlay({ message }) {
   );
 }
 
-export default LoadingOverlay;
-
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   message: {
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
+import { LoadingOverlay } from "../components/ui/LoadingOverlay";
 import { createUser } from "../utils/auth";
 
 function SignupScreen() {
-  const signupHandler = ({ email, password }) => {
-    createUser(email, password);
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const signupHandler = async ({ email, password }) => {
+    setIsAuthenticating(true);
+    await createUser(email, password);
+    setIsAuthenticating(false);
   };
+
+  if (isAuthenticating) return <LoadingOverlay message="Creating user..." />;
 
   return <AuthContent onAuthenticate={signupHandler} />;
 }
