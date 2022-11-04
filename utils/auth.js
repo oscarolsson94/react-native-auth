a320e60da7ff398bf6f1c87ac3968fc570ceab18
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-11-04 15:38:22 +0100

    adding api key

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,9 @@
 import axios from "axios";
 
-const createUser = () => {};
+const API_KEY = AIzaSyBrASPUke7dfoIIWPB78uLPAZBouGXQITU;
+
+const createUser = () => {
+  axios.post(
+    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`
+  );
+};
