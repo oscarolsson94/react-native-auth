457758bec8713951fb8410add23f71bb993c42a8
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-11-07 10:26:41 +0100

    cond rendering stacks

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import { Colors } from "./constants/styles";
-import { AuthContextProvider } from "./store/auth-context";
+import { AuthContext, AuthContextProvider } from "./store/auth-context";
+import { useContext } from "react";
 
 const Stack = createNativeStackNavigator();
 
@@ -40,12 +41,11 @@ function AuthenticatedStack() {
 }
 
 function Navigation() {
+  const { isAuthenticated } = useContext(AuthContext);
   return (
-    <AuthContextProvider>
-      <NavigationContainer>
-        <AuthStack />
-      </NavigationContainer>
-    </AuthContextProvider>
+    <NavigationContainer>
+      {!isAuthenticated ? <AuthStack /> : <AuthenticatedStack />}
+    </NavigationContainer>
   );
 }
 
@@ -53,8 +53,9 @@ export default function App() {
   return (
     <>
       <StatusBar style="light" />
-
-      <Navigation />
+      <AuthContextProvider>
+        <Navigation />
+      </AuthContextProvider>
     </>
   );
 }
