4705eff979f23d0a29b45dd8fb0743e2b758dca0
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-11-07 11:16:34 +0100

    using token to reach authenticated routes

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+
+  const { token } = useContext(AuthContext);
+  useEffect(() => {
+    axios
+      .get(
+        "https://react-native-course-7ae38-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=" +
+          token
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      });
+  }, [token]);
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
@@ -14,13 +31,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
