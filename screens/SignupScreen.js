8bcf1845749a632aa90ad923b6eb5702dc67aeab
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-11-07 10:15:43 +0100

    setting up authcontext

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 import AuthContent from "../components/Auth/AuthContent";
 import { LoadingOverlay } from "../components/ui/LoadingOverlay";
 import { createUser } from "../utils/auth";
@@ -8,7 +9,11 @@ function SignupScreen() {
 
   const signupHandler = async ({ email, password }) => {
     setIsAuthenticating(true);
-    await createUser(email, password);
+    try {
+      await createUser(email, password);
+    } catch (error) {
+      Alert.alert("Authentication failed", "Could not craete user..");
+    }
     setIsAuthenticating(false);
   };
 
