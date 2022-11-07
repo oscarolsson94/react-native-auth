7ca75dfd2a045813ee44e66e4a87d2fa76f054b5
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-11-07 09:55:01 +0100

    creating signup and login api calls

--- utils/auth.js
+++ utils/auth.js
@@ -2,13 +2,20 @@ import axios from "axios";
 
 const API_KEY = AIzaSyBrASPUke7dfoIIWPB78uLPAZBouGXQITU;
 
+export const authenticate = async (mode, email, password) => {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email,
+    password,
+    returnSecureToken: true,
+  });
+};
+
 export const createUser = async (email, password) => {
-  const response = axios.post(
-    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
-    {
-      email,
-      password,
-      returnSecureToken: true,
-    }
-  );
+  const response = await authenticate("signUp", email, password);
+};
+
+export const login = async (email, password) => {
+  const response = await authenticate("signInWithPassword", email, password);
 };
